f7f08203441d6c783d2bebb624b12769aba6391c
ThanushaJ <thanusnani@gmail.com>
2020-05-10 14:08:19 +0400

    render some content

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+//Create a React component
+const App = () => {
+  return <div>Hello, I'm React</div>;
+};
+//Take the react component and show it on the screen
+//render is a function which takes arguments
+//first argument - <App/> is JSX of the component we created
+//second argument is reference to "root" element of index.html
+ReactDOM.render(<App />, document.querySelector("#root"));
